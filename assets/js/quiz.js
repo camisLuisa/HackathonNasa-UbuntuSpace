91c39376abe89fd4ffe2126f2432ee9c03af1935
camila.farias <camila.farias@mv.com.br>
2020-10-04 09:57:19 -0300

    Menu e fonts

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,20 +1,21 @@
 class Inventor {
-    constructor(id, question, description, font) {
+    constructor(id, question, description, fontOne, fontTwo) {
       this.id = id
       this.question = question
       this.description = description
-      this.font = font
+      this.fontOne = fontOne
+      this.fontTwo = fontTwo
     }
 }
 
 var infoElement = document.getElementById('information');
 infoElement.style.display = "none";
 var count = 0
-const inventorOne = new Inventor(1, "Considerado por muitos o maior nome da literatura do Brasil:", "Machado de Assis (1839-1908) foi um escritor brasileiro, um dos nomes mais importantes da literatura do século XIX. Escreveu poesias, contos, e romances. Foi também jornalista, teatrólogo, crítico de teatro e crítico literário. Ele presidiu a Academia Brasileira de letras, da qual é membro fundador. Suas obras foram traduzidas para várias línguas.", "https://www.ebiografia.com/vida_obra_principais_escritores_romantismo_brasileiro/https://www.ebiografia.com/machado_assis/")
-const inventorTwo = new Inventor(2, "Cientista da NASA que recebeu a Medalha Presidencial da Liberdade, a maior condecoração civil dos Estados Unidos:", "Katherine Johnson (1918-2020), matemática da NASA, com uma capacidade extraordinária para cálculos, contribuiu para colocar em órbita a Apolo 11, a nave que levou o homem à Lua pela primeira vez.", "https://www.paramulheresnaciencia.com.br/noticias/quem-e-a-mulher-que-descobriu-o-primeiro-coronavirus-humano-conheca-a-cientista-june-almeida/")
-const inventorThree = new Inventor(3, "Ganhador do Prêmio Nobel da Paz em 1964:", "Martin Luther King (1929-1968) foi um ativista norte-americano, lutou contra a discriminação racial e tornou-se um dos mais importantes líderes dos movimentos pelos direitos civis dos negros nos Estados Unidos.", "https://www.thewrap.com/james-drury-the-virginian-star-dies-at-85/")
-const inventorFour = new Inventor(4, "De acordo com a revista Forbes, ela  foi eleita a mulher mais rica do ramo de entretenimento no mundo durante o século XX:", "Oprah Winfrey (1954-) é uma apresentadora, jornalista, atriz, psicóloga, empresária, repórter, produtora, editora e escritora norte-americana, vencedora de múltiplos prêmios Emmy por seu programa The Oprah Winfrey Show, o talk show com maior audiência da história da televisão norte-americana.", "https://pt.wikipedia.org/wiki/Oprah_Winfrey")
-const inventorFive = new Inventor(5, "Presidente dos Estados Unidos de 2009 a 2017:", "Barack Obama II (1961 - ) é um advogado e político norte-americano que serviu como o 44.º presidente dos Estados Unidos de 2009 a 2017, sendo o primeiro afro-americano a ocupar o cargo. Nascido em Honolulu, no Havaí, Obama é graduado em ciência política pela Universidade Columbia e em direito pela Universidade de Harvard, onde foi presidente da Harvard Law Review.", "https://pt.wikipedia.org/wiki/Barack_Obama")
+const inventorOne = new Inventor(1, "Considerado por muitos o maior nome da literatura do Brasil:", "Machado de Assis (1839-1908) foi um escritor brasileiro, um dos nomes mais importantes da literatura do século XIX. Escreveu poesias, contos, e romances. Foi também jornalista, teatrólogo, crítico de teatro e crítico literário. Ele presidiu a Academia Brasileira de letras, da qual é membro fundador. Suas obras foram traduzidas para várias línguas.", "https://www.ebiografia.com/machado_assis/", "https://www.ebiografia.com/vida_obra_principais_escritores_romantismo_brasileiro/")
+const inventorTwo = new Inventor(2, "Cientista da NASA que recebeu a Medalha Presidencial da Liberdade, a maior condecoração civil dos Estados Unidos:", "Katherine Johnson (1918-2020), matemática da NASA, com uma capacidade extraordinária para cálculos, contribuiu para colocar em órbita a Apolo 11, a nave que levou o homem à Lua pela primeira vez.", "https://www.paramulheresnaciencia.com.br/noticias/quem-e-a-mulher-que-descobriu-o-primeiro-coronavirus-humano-conheca-a-cientista-june-almeida/", "https://www.nasa.gov/content/katherine-johnson-biography")
+const inventorThree = new Inventor(3, "Ganhador do Prêmio Nobel da Paz em 1964:", "Martin Luther King (1929-1968) foi um ativista norte-americano, lutou contra a discriminação racial e tornou-se um dos mais importantes líderes dos movimentos pelos direitos civis dos negros nos Estados Unidos.", "https://www.thewrap.com/james-drury-the-virginian-star-dies-at-85/", "https://www.ebiografia.com/martin_luther_king/")
+const inventorFour = new Inventor(4, "De acordo com a revista Forbes, ela  foi eleita a mulher mais rica do ramo de entretenimento no mundo durante o século XX:", "Oprah Winfrey (1954-) é uma apresentadora, jornalista, atriz, psicóloga, empresária, repórter, produtora, editora e escritora norte-americana, vencedora de múltiplos prêmios Emmy por seu programa The Oprah Winfrey Show, o talk show com maior audiência da história da televisão norte-americana.", "https://pt.wikipedia.org/wiki/Oprah_Winfrey", "https://pt.wikipedia.org/wiki/Joan_Rivers")
+const inventorFive = new Inventor(5, "Presidente dos Estados Unidos de 2009 a 2017:", "Barack Obama II (1961 - ) é um advogado e político norte-americano que serviu como o 44.º presidente dos Estados Unidos de 2009 a 2017, sendo o primeiro afro-americano a ocupar o cargo. Nascido em Honolulu, no Havaí, Obama é graduado em ciência política pela Universidade Columbia e em direito pela Universidade de Harvard, onde foi presidente da Harvard Law Review.", "https://pt.wikipedia.org/wiki/Barack_Obama", "https://pt.wikipedia.org/wiki/John_McCain")
 var inventors = [inventorOne, inventorTwo, inventorThree, inventorFour, inventorFive]
 
 function getInfoFromInventos() {
@@ -23,6 +24,8 @@ function getInfoFromInventos() {
     var imgOne = window.document.getElementById('imgOne')
     var imgTwo = window.document.getElementById('imgTwo')
     var rightImg = window.document.getElementById('rightImg')
+    var refOne = window.document.getElementById('linkOne')
+    var refTwo = window.document.getElementById('linkTwo')
 
     question.innerText = inventors[count].question
     description.innerText = inventors[count].description
@@ -31,6 +34,9 @@ function getInfoFromInventos() {
     imgTwo.src = "images/photos/"+inventors[count].id+"F.jpeg"
     imgTwo.title = inventors[count].question + "F"
     rightImg.src = "images/photos/"+inventors[count].id+".jpeg"
+
+    refOne.href = inventors[count].fontOne
+    refTwo.href = inventors[count].fontTwo
   }
 
   getInfoFromInventos()
